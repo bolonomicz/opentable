a2059978b8233e22a5658d17837d50f3ca335228
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-19 15:15:06 -0400

    owner model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opentable::Application.config.secret_token = '002c65d001e9089603b0411e1ed0919d4ba4f76921f350609df1c80725f509af20e9c4ed43fedfccad583bbd2859077d309c79a6df60539889ca7d2064747904'
